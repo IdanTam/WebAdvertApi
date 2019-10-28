38a7a1a9ef9c8e0d4f8944599228a88371da347b
Idan Tam <Idan.Tam@global-e.com>
2019-10-28 18:36:23 +0200

    API

---
+++ AdvertApi/Services/AdvertDbModel.cs
@@ -0,0 +1,17 @@
+﻿using AdvertApi.Models;
+using System;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AdvertApi.Services
+{
+
+    public class AdvertDbModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public AdvertStatus Status { get; set; }
+    }
+}
---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        public Task<string> Add(AdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
