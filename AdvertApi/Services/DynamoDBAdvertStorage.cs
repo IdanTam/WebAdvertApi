617444ecccbb4be0356515d4bf0fac42c1d0b7b5
Idan Tam <Idan.Tam@global-e.com>
2019-10-29 15:27:39 +0200

    Contollers

--- AdvertApi/Services/DynamoDBAdvertStorage.cs
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -3,19 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AdvertApi.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using AutoMapper;
 
 namespace AdvertApi.Services
 {
     public class DynamoDBAdvertStorage : IAdvertStorageService
     {
-        public Task<string> Add(AdvertModel model)
+        private readonly IMapper _mapper;
+
+        public DynamoDBAdvertStorage(IMapper mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
         }
 
-        public Task<bool> Confirm(ConfirmAdvertModel model)
+        public async Task<string> Add(AdvertModel model)
         {
-            throw new NotImplementedException();
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+            dbModel.Id = new Guid().ToString();
+            dbModel.CreationDateTime = DateTime.UtcNow;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using ( var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbModel);
+                }
+            }
+
+            return dbModel.Id;
+        }
+
+        public async Task Confirm(ConfirmAdvertModel model)
+        {
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    var record = await context.LoadAsync<AdvertDbModel>(model.Id);
+
+                    if (record == null)
+                    {
+                        throw new KeyNotFoundException($"A record witg ID={model.Id} was not found");
+                    }
+                    if (model.Status == AdvertStatus.Active)
+                    {
+                        record.Status = AdvertStatus.Active;
+                    }
+                    else
+                    {
+                        await context.DeleteAsync(record);
+                    }
+                   
+                }
+            }
         }
     }
 }
--- AdvertApi/Startup.cs
+++ AdvertApi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using AutoMapper;
+using AdvertApi.Services;
 
 namespace AdvertApi
 {
@@ -24,6 +26,8 @@ namespace AdvertApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
+            services.AddTransient<IAdvertStorageService, DynamoDBAdvertStorage>();
             services.AddControllers();
         }
 
